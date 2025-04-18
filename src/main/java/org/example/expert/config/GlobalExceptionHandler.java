6b6a4bd138600f4865b614757d9fbdb2c2fa4cea
pleasebelieveme <cain1227@naver.com>
2025-04-18 17:36:08 +0900

    레벨1-3: 밸리데이션 리펙토링

--- src/main/java/org/example/expert/config/GlobalExceptionHandler.java
+++ src/main/java/org/example/expert/config/GlobalExceptionHandler.java
@@ -5,6 +5,8 @@ import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.common.exception.ServerException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -39,6 +41,12 @@ public class GlobalExceptionHandler {
         errorResponse.put("message", message);
 
         return new ResponseEntity<>(errorResponse, status);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationException(MethodArgumentNotValidException ex) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        return getErrorResponse(status, ex.getMessage());
     }
 }
 
--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,8 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +14,13 @@ public class UserChangePasswordRequest {
 
     @NotBlank
     private String oldPassword;
+
     @NotBlank
+    @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
+    // Spring Boot 3.x이상부터는 jakarta import
+    @Pattern(
+        regexp = "^(?=.*[A-Z])(?=.*\\d).*$",
+        message = "비밀번호는 최소 하나의 대문자와 숫자를 포함해야 합니다."
+    )
     private String newPassword;
 }
